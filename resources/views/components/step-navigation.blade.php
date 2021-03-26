a011c262c527acc74039484058b5ec0b1d5ef96d
Алексей Похил <AlekcukP@gmail.com>
2021-03-26 19:36:48 +0200

    Added step-navigation component

---
+++ resources/views/components/step-navigation.blade.php
@@ -0,0 +1,16 @@
+<nav class="steps_nav">
+    <ul class="steps_list">
+        <li class="steps_item">
+            <span>Personal info</span>
+        </li>
+        <li class="steps_item">
+            <span>Your home</span>
+        </li>
+        <li class="steps_item">
+            <span>Materials</span>
+        </li>
+        <li class="steps_item">
+            <span>Extras</span>
+        </li>
+    </ul>
+</nav>
